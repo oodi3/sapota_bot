f3b716856e2df81f99d3ff8f905f760a85a0683f
oodi <odmin.de@gmail.com>
2024-07-29 02:30:49 +0300

    add download func

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 import telebot
 from telebot import types
 from res import response
+import yt_dlp
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -32,6 +33,44 @@ def start(message):
     bot.send_message(
         message.chat.id, f" Hi ! How can I help you ? ", reply_markup=keyboard
     )
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'download_video')
+def handle_download_video(call):
+    bot.send_message(call.message.chat.id, "Send me the video or playlist link you want to download! 😸")
+    bot.register_next_step_handler(call.message, process_video_download)
+
+
+def process_video_download(message):
+    url = message.text
+    try:
+        bot.send_message(message.chat.id, "Downloading video(s)... 😸")
+        video_paths = download_videos(url)
+
+        bot.send_message(message.chat.id, "Download completed! 😊 Your video(s) are saved locally.")
+    except Exception as e:
+        bot.send_message(message.chat.id, f"Failed to download video(s). 😿 Error: {e}")
+
+
+def download_videos(url, output_dir=SAVE_DIR):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    ydl_opts = {
+        'format': 'bestvideo+bestaudio/best',  # Скачиваем лучшее видео и аудио
+        'outtmpl': f'{output_dir}/%(title)s.%(ext)s',
+        'noplaylist': False  # Разрешаем скачивание плейлистов
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    # Возвращаем список скачанных файлов
+    downloaded_files = []
+    for root, _, files in os.walk(output_dir):
+        for file in files:
+            downloaded_files.append(os.path.join(root, file))
+    return downloaded_files
 
 
 @bot.message_handler()
